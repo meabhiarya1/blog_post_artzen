9b2f023d646316de32d7e9378aa00fe7406432de
Abhishek kumar <abhi.arya97161@gmail.com>
2025-05-01 10:29:48 +0530

    refactor: enhance post handling logic and improve dialog management in PostTable component

--- client/src/Componenets/PostTable/PostTable.jsx
+++ client/src/Componenets/PostTable/PostTable.jsx
@@ -17,50 +17,56 @@ import { useNavigate } from "react-router-dom";
 
 const PostTable = () => {
   const [posts, setPosts] = useState();
-  const [isAdd, setIsAdd] = useState(false);
-  const [editData, setEditData] = useState(null);
-  const [isEditing, setIsEditing] = useState(false);
-  const [modelInfo, setModelInfo] = useState({ title: "", buttonLabel: "" });
+  const [isAddMode, setIsAddMode] = useState(false);
+  const [postFormData, setPostFormData] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [dialogInfo, setDialogInfo] = useState({ title: "", buttonLabel: "" });
   const apiUrl = import.meta.env.VITE_API_URL;
   const navigate = useNavigate();
 
-  async function newFun() {
+  const fetchPosts = async () => {
     try {
-      const respone = await axios.get(`${apiUrl}/api/v1/posts`);
-      setPosts(respone.data);
+      const response = await axios.get(`${apiUrl}/api/v1/posts`);
+      setPosts(response.data);
     } catch (error) {
-      console.error("Error getting data:", error);
+      console.error("Error fetching posts:", error);
     }
-  }
-
-  useEffect(() => {
-    newFun();
-  }, []);
-
-  const handleEdit = (row) => {
-    setModelInfo({ title: "Edit Post", buttonLabel: "UPDATE Post" });
-    setEditData(row);
-    setIsAdd(false);
-    setIsEditing(true);
   };
 
-  const handleDelete = async (id) => {
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const openEditPostDialog = (post) => {
+    setDialogInfo({ title: "Edit Post", buttonLabel: "Update Post" });
+    setPostFormData(post);
+    setIsAddMode(false);
+    setIsModalOpen(true);
+  };
+
+  const openAddPostDialog = () => {
+    setDialogInfo({ title: "Add Post", buttonLabel: "Add Post" });
+    setPostFormData({ title: "", content: "", author: "" });
+    setIsAddMode(true);
+    setIsModalOpen(true);
+  };
+
+  const deletePost = async (id) => {
     try {
       const response = await axios.delete(`${apiUrl}/api/v1/posts/${id}`);
-      newFun();
+      fetchPosts();
       toast.success(response.data.message);
     } catch (error) {
       toast.error(error.response?.data?.error);
     }
   };
 
-  const handleAddPost = async (data) => {
+  const addPost = async (data) => {
     try {
       const response = await axios.post(`${apiUrl}/api/v1/posts`, data);
       if (response) {
-        newFun();
-        setIsEditing(false);
-        setEditData(null);
+        fetchPosts();
+        closeDialog();
         toast.success(response.data.message);
       }
     } catch (error) {
@@ -68,50 +74,43 @@ const PostTable = () => {
     }
   };
 
-  const handleEditPost = async (data) => {
+  const updatePost = async (data) => {
     try {
       const response = await axios.patch(
         `${apiUrl}/api/v1/posts/${data.id}`,
         data
       );
-      newFun();
-      setIsEditing(false);
-      setEditData(null);
+      fetchPosts();
+      closeDialog();
       toast.success(response.data.message);
     } catch (error) {
       toast.error(error.response?.data?.error);
     }
   };
 
-  const handleSubmit = (data) => {
-    if (!data.title || data.title === "") {
+  const handleFormSubmit = (data) => {
+    if (!data.title?.trim()) {
       toast.error("Title is required");
       return;
     }
-    if (!data.content || data.content === "") {
+    if (!data.content?.trim()) {
       toast.error("Content is required");
       return;
     }
-    if (!data.author || data.author === "") {
+    if (!data.author?.trim()) {
       toast.error("Author is required");
       return;
     }
 
-    if (isAdd) {
-      handleAddPost(data);
-    } else {
-      handleEditPost(data);
-    }
+    isAddMode ? addPost(data) : updatePost(data);
   };
 
-  const handleOpenAddModel = () => {
-    setModelInfo({ title: "Add Post ", buttonLabel: "ADD " });
-    setEditData({ title: "", content: "", author: "" });
-    setIsAdd(true);
-    setIsEditing(true);
+  const closeDialog = () => {
+    setIsModalOpen(false);
+    setPostFormData(null);
   };
 
-  const tableColumns = [
+  const columns = [
     {
       name: "Title",
       selector: (row) => row.title,
@@ -134,22 +133,23 @@ const PostTable = () => {
       name: "Actions",
       cell: (row) => (
         <div>
-          <EditIcon className="editIcon" onClick={() => handleEdit(row)} />
-          <DeleteIcon onClick={() => handleDelete(row.id)} />
+          <EditIcon
+            className="editIcon"
+            onClick={() => openEditPostDialog(row)}
+          />
+          <DeleteIcon onClick={() => deletePost(row.id)} />
         </div>
       ),
       width: "250px",
     },
   ];
 
-  const tableDataItems = posts?.map((post) => {
-    return {
-      title: post.title,
-      content: post.content,
-      author: post.author,
-      id: post._id,
-    };
-  });
+  const dataItems = posts?.map((post) => ({
+    title: post.title,
+    content: post.content,
+    author: post.author,
+    id: post._id,
+  }));
 
   return (
     <>
@@ -157,7 +157,7 @@ const PostTable = () => {
         <div className="w-full flex justify-end mb-5">
           <button
             className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-blue-600 transition"
-            onClick={handleOpenAddModel}
+            onClick={openAddPostDialog}
           >
             Add Post
           </button>
@@ -165,8 +165,8 @@ const PostTable = () => {
 
         <div className="w-full max-w-8xl bg-white shadow-lg rounded-lg p-6">
           <DataTable
-            data={tableDataItems}
-            columns={tableColumns}
+            data={dataItems}
+            columns={columns}
             pagination
             highlightOnHover
             fixedHeader
@@ -175,15 +175,15 @@ const PostTable = () => {
           />
         </div>
 
-        <Dialog open={isEditing} onClose={() => setIsEditing(false)}>
+        <Dialog open={isModalOpen} onClose={closeDialog}>
           <DialogTitle className="text-xl font-semibold text-gray-900">
-            {modelInfo.title}
+            {dialogInfo.title}
           </DialogTitle>
           <DialogContent>
             <form
               onSubmit={(e) => {
                 e.preventDefault();
-                handleSubmit(editData);
+                handleFormSubmit(postFormData);
               }}
               className="space-y-4"
             >
@@ -193,9 +193,9 @@ const PostTable = () => {
                 type="text"
                 fullWidth
                 variant="outlined"
-                value={editData?.title || ""}
+                value={postFormData?.title || ""}
                 onChange={(e) =>
-                  setEditData({ ...editData, title: e.target.value })
+                  setPostFormData({ ...postFormData, title: e.target.value })
                 }
               />
               <TextField
@@ -204,9 +204,9 @@ const PostTable = () => {
                 type="text"
                 fullWidth
                 variant="outlined"
-                value={editData?.content || ""}
+                value={postFormData?.content || ""}
                 onChange={(e) =>
-                  setEditData({ ...editData, content: e.target.value })
+                  setPostFormData({ ...postFormData, content: e.target.value })
                 }
               />
               <TextField
@@ -215,25 +215,22 @@ const PostTable = () => {
                 type="text"
                 fullWidth
                 variant="outlined"
-                value={editData?.author || ""}
+                value={postFormData?.author || ""}
                 onChange={(e) =>
-                  setEditData({ ...editData, author: e.target.value })
+                  setPostFormData({ ...postFormData, author: e.target.value })
                 }
               />
             </form>
           </DialogContent>
           <DialogActions className="p-4">
-            <Button
-              onClick={() => setIsEditing(false)}
-              className="text-red-500"
-            >
+            <Button onClick={closeDialog} className="text-red-500">
               Cancel
             </Button>
             <Button
-              onClick={() => handleSubmit(editData)}
+              onClick={() => handleFormSubmit(postFormData)}
               className="bg-green-500 text-white font-semibold px-4 py-2 rounded-lg shadow-md hover:bg-green-600 transition"
             >
-              {modelInfo.buttonLabel}
+              {dialogInfo.buttonLabel}
             </Button>
           </DialogActions>
         </Dialog>
