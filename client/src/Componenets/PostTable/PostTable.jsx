f94f9acbf6ba921cc337991bf01f96c14d8b4107
Abhishek kumar <abhi.arya97161@gmail.com>
2025-05-01 10:27:53 +0530

    refactor: streamline post handling functions and improve form validation in PostTable component

--- client/src/Componenets/PostTable/PostTable.jsx
+++ client/src/Componenets/PostTable/PostTable.jsx
@@ -14,12 +14,12 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+
 const PostTable = () => {
   const [posts, setPosts] = useState();
-  // const [report, setReport] = useState([]);
   const [isAdd, setIsAdd] = useState(false);
-  const [editData, setEditData] = useState(null); // To manage edit state
-  const [isEditing, setIsEditing] = useState(false); // To toggle between add and edit modes
+  const [editData, setEditData] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
   const [modelInfo, setModelInfo] = useState({ title: "", buttonLabel: "" });
   const apiUrl = import.meta.env.VITE_API_URL;
   const navigate = useNavigate();
@@ -28,7 +28,6 @@ const PostTable = () => {
     try {
       const respone = await axios.get(`${apiUrl}/api/v1/posts`);
       setPosts(respone.data);
-      // setReport(respone.data);
     } catch (error) {
       console.error("Error getting data:", error);
     }
@@ -37,6 +36,80 @@ const PostTable = () => {
   useEffect(() => {
     newFun();
   }, []);
+
+  const handleEdit = (row) => {
+    setModelInfo({ title: "Edit Post", buttonLabel: "UPDATE Post" });
+    setEditData(row);
+    setIsAdd(false);
+    setIsEditing(true);
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      const response = await axios.delete(`${apiUrl}/api/v1/posts/${id}`);
+      newFun();
+      toast.success(response.data.message);
+    } catch (error) {
+      toast.error(error.response?.data?.error);
+    }
+  };
+
+  const handleAddPost = async (data) => {
+    try {
+      const response = await axios.post(`${apiUrl}/api/v1/posts`, data);
+      if (response) {
+        newFun();
+        setIsEditing(false);
+        setEditData(null);
+        toast.success(response.data.message);
+      }
+    } catch (error) {
+      toast.error(error.response?.data?.error);
+    }
+  };
+
+  const handleEditPost = async (data) => {
+    try {
+      const response = await axios.patch(
+        `${apiUrl}/api/v1/posts/${data.id}`,
+        data
+      );
+      newFun();
+      setIsEditing(false);
+      setEditData(null);
+      toast.success(response.data.message);
+    } catch (error) {
+      toast.error(error.response?.data?.error);
+    }
+  };
+
+  const handleSubmit = (data) => {
+    if (!data.title || data.title === "") {
+      toast.error("Title is required");
+      return;
+    }
+    if (!data.content || data.content === "") {
+      toast.error("Content is required");
+      return;
+    }
+    if (!data.author || data.author === "") {
+      toast.error("Author is required");
+      return;
+    }
+
+    if (isAdd) {
+      handleAddPost(data);
+    } else {
+      handleEditPost(data);
+    }
+  };
+
+  const handleOpenAddModel = () => {
+    setModelInfo({ title: "Add Post ", buttonLabel: "ADD " });
+    setEditData({ title: "", content: "", author: "" });
+    setIsAdd(true);
+    setIsEditing(true);
+  };
 
   const tableColumns = [
     {
@@ -61,11 +134,8 @@ const PostTable = () => {
       name: "Actions",
       cell: (row) => (
         <div>
-          <EditIcon
-            className="editIcon"
-            onClick={() => handleEdit(row)}
-          ></EditIcon>
-          <DeleteIcon onClick={() => handleDelete(row.id)}></DeleteIcon>
+          <EditIcon className="editIcon" onClick={() => handleEdit(row)} />
+          <DeleteIcon onClick={() => handleDelete(row.id)} />
         </div>
       ),
       width: "250px",
@@ -77,77 +147,13 @@ const PostTable = () => {
       title: post.title,
       content: post.content,
       author: post.author,
-      //   "manage": ,
       id: post._id,
     };
   });
 
-  const handleEdit = (row) => {
-    setModelInfo({ title: "Edit Post", buttonLabel: "UPDATE Post" });
-    setEditData(row);
-
-    setIsEditing(true);
-  };
-
-  const handleDelete = async (id) => {
-    try {
-      const response = await axios.delete(`${apiUrl}/api/v1/posts/${id}`);
-      newFun();
-      toast.success(response.data.message);
-    } catch (error) {
-      toast.error(error.response?.data?.error);
-    }
-  };
-
-  const handleAddUpdate = async (data) => {
-    try {
-      if (!data.title || data.title === "") {
-        toast.error("Title is required");
-        return;
-      }
-      if (!data.content || data.content === "") {
-        toast.error("Content is required");
-        return;
-      }
-      if (!data.author || data.author === "") {
-        toast.error("Author is required");
-        return;
-      }
-      console.log(isAdd);
-      if (isAdd) {
-        const response = await axios.post(`${apiUrl}/api/v1/posts`, data);
-        if (response) {
-          newFun();
-          setIsEditing(false);
-          setEditData(null);
-          toast.success(response.data.message);
-        }
-      } else {
-        const response = await axios.patch(
-          `${apiUrl}/api/v1/posts/${data.id}`,
-          data
-        );
-        newFun();
-        setIsEditing(false);
-        setEditData(null);
-        toast.success(response.data.message);
-      }
-    } catch (error) {
-      toast.error(error.response?.data?.error);
-    }
-  };
-
-  const handleOpenAddModel = () => {
-    setModelInfo({ title: "Add Post ", buttonLabel: "ADD " });
-    setEditData("");
-    // setIsAdd(true);
-    setIsEditing(true);
-  };
-
   return (
     <>
       <div className="flex flex-col items-center justify-center max-h-screen bg-gray-100 p-6">
-        {/* Add Post Button - Right Aligned */}
         <div className="w-full flex justify-end mb-5">
           <button
             className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-blue-600 transition"
@@ -157,7 +163,6 @@ const PostTable = () => {
           </button>
         </div>
 
-        {/* Table Container */}
         <div className="w-full max-w-8xl bg-white shadow-lg rounded-lg p-6">
           <DataTable
             data={tableDataItems}
@@ -170,7 +175,6 @@ const PostTable = () => {
           />
         </div>
 
-        {/* Dialog Modal */}
         <Dialog open={isEditing} onClose={() => setIsEditing(false)}>
           <DialogTitle className="text-xl font-semibold text-gray-900">
             {modelInfo.title}
@@ -179,7 +183,7 @@ const PostTable = () => {
             <form
               onSubmit={(e) => {
                 e.preventDefault();
-                handleAddUpdate(editData);
+                handleSubmit(editData);
               }}
               className="space-y-4"
             >
@@ -193,7 +197,6 @@ const PostTable = () => {
                 onChange={(e) =>
                   setEditData({ ...editData, title: e.target.value })
                 }
-                className="border rounded-md p-2 w-full"
               />
               <TextField
                 margin="dense"
@@ -205,7 +208,6 @@ const PostTable = () => {
                 onChange={(e) =>
                   setEditData({ ...editData, content: e.target.value })
                 }
-                className="border rounded-md p-2 w-full"
               />
               <TextField
                 margin="dense"
@@ -217,7 +219,6 @@ const PostTable = () => {
                 onChange={(e) =>
                   setEditData({ ...editData, author: e.target.value })
                 }
-                className="border rounded-md p-2 w-full"
               />
             </form>
           </DialogContent>
@@ -229,7 +230,7 @@ const PostTable = () => {
               Cancel
             </Button>
             <Button
-              onClick={() => handleAddUpdate(editData)}
+              onClick={() => handleSubmit(editData)}
               className="bg-green-500 text-white font-semibold px-4 py-2 rounded-lg shadow-md hover:bg-green-600 transition"
             >
               {modelInfo.buttonLabel}
